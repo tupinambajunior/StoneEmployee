7d787f2dc690ebb5b8427f655f1e78a029681699
tupinambajunior <tupinambajunior2@gmail.com>
2024-06-09 17:42:20 -0300

    Configuration for table employee and context entity create

---
+++ StoneEmployee.Infrastructure/Database/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StoneEmployee.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneEmployee.Infrastructure.Database.Configuration
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder
+               .ToTable("employee");
+
+            builder
+                .Property(x => x.Id)
+                .HasColumnName("id")
+                .HasMaxLength(100);
+
+            builder
+                .Property(x => x.FirstName)
+                .HasColumnName("first_name")
+                .HasMaxLength(255);
+
+            builder
+                .Property(x => x.LastName)
+                .HasColumnName("last_name")
+                .HasMaxLength(255);
+
+            builder
+                .Property(x => x.Document)
+                .HasColumnName("document")
+                .HasMaxLength(11);
+
+            builder
+                .Property(x => x.Sector)
+                .HasColumnName("sector")
+                .HasMaxLength(255);
+
+            builder
+                .Property(x => x.GrossSalary)
+                .HasColumnName("gross_salary");
+
+            builder
+                .Property(x => x.AdmissionDate)
+                .HasColumnName("admission_data");
+
+            builder
+                .Property(x => x.HasHealthPlan)
+                .HasColumnName("has_health_plan");
+
+            builder
+                .Property(x => x.HasDentalPlan)
+                .HasColumnName("has_dental_plan");
+
+            builder
+                .Property(x => x.HasTransportationVouchers)
+                .HasColumnName("has_transportation_vouchers");
+        }
+    }
+}
---
+++ StoneEmployee.Infrastructure/Database/PgContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoneEmployee.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneEmployee.Infrastructure.Database
+{
+    public class PgContext : DbContext
+    {
+        public PgContext(DbContextOptions<PgContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
